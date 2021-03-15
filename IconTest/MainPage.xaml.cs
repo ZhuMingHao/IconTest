c22934b256deae5e7bbf6c116322ee53bc33df79
Nico Zhu <v-zhumin@microsoft.com>
2021-03-15 15:59:47 +0800

    Add project files.

---
+++ IconTest/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IconTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void GetThumbnail()
+        {
+            var openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add("*");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            using (StorageItemThumbnail thumbnail = await file.GetThumbnailAsync(ThumbnailMode.VideosView, 250)) //Do not use PictureVeiw which show only partial cropped picture thumbnail.
+            {
+
+                //---------- Save thumbnail image
+                var bitmapImg = new BitmapImage();
+                await bitmapImg.SetSourceAsync(thumbnail);
+
+                iconImage.Source = bitmapImg;
+
+            }
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetThumbnail();
+        }
+
+        private void OnItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
